bb059e0b45f09f6251bb4f52b3e1c8dccaab668f
Rocky D. Pulley <rockytriton@yahoo.com>
2018-12-09 22:42:46 -0600

    fixed bootstrap bar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./styles/style.scss";
 import "bootstrap/scss/bootstrap.scss"
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import { App } from "./components/App";
 
 const ROOT = document.querySelector(".container");
