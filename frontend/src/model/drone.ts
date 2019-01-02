092defa1591c242d7d0aa95190896b093c0a8c8a
rockytriton <rockytriton@yahoo.com>
2019-01-01 22:06:34 -0600

    models

---
+++ frontend/src/model/drone.ts
@@ -0,0 +1,12 @@
+
+export default class Drone
+{
+    id : number;
+    fk_type_id : number;
+    typeName : string;
+    location_lat : number;
+    location_lon : number;
+    weight : number;
+    status : number;
+}
+
---
+++ frontend/src/model/droneType.ts
@@ -0,0 +1,9 @@
+export default class DroneType
+{
+    id : number;
+    name : string;
+    manufacturer : string;
+    max_weight : number;
+    max_packages : number;
+}
+
